53834a67d957635d4d63d6a6692e3f3e715d736f
pablinchapin <pablinchapin@gmail.com>
2017-02-24 11:40:48 -0600

    Inicializacion de reporte

---
+++ src/main/java/com/pablinchapin/tiendaliz/PcSkeletonTiendalizCApplication.java
@@ -0,0 +1,12 @@
+package com.pablinchapin.tiendaliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcSkeletonTiendalizCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcSkeletonTiendalizCApplication.class, args);
+	}
+}
